dd4a01c2b3c70daa362fdd0eee4cd6a13e286598
csgarlock <csgarlock@gmail.com>
2025-08-29 11:07:24 -0400

    magic seed gen

--- src/magic.rs
+++ src/magic.rs
@@ -1,4 +1,7 @@
-use rand::{rng, rngs, RngCore};
+use std::u64;
+
+use rand::{rng, RngCore, SeedableRng};
+use rand_xoshiro::Xoroshiro128PlusPlus;
 
 use crate::{bitboard::{bit_count, board_from_square, file, pretty_string_bitboard, rank, Bitboard, Square, EMPTY_BITBOARD, FILE0, FILE7, FILES, RANK0, RANK7, RANKS}, piece_info::{can_step, make_step, PieceType, MOVE_BOARDS}};
 
@@ -46,23 +49,28 @@ pub static mut BISHOP_TABLE: [Bitboard; 0x1480] = [EMPTY_BITBOARD; 0x1480];
 pub static mut ROOK_TABLE: [Bitboard; 0x19000] = [EMPTY_BITBOARD; 0x19000];
 
 pub fn magic_init() {
+    const MAGIC_SEED: [u8; 16] = [55, 75, 133, 217, 115, 71, 255, 96, 162, 5, 252, 80, 205, 244, 116, 201];
+    let mut rng = Xoroshiro128PlusPlus::from_seed(MAGIC_SEED);
     let mut running_rook_offset: usize = 0;
     let mut running_bishop_offset: usize = 0;
+    let mut running_total = 0u64;
     for square in 0..64 {
         unsafe {
-            let rook_offset= find_magic::<{ PieceType::Rook }>(square, &mut ROOK_MAGICS[square as usize], &mut ROOK_TABLE[running_rook_offset..]);
-            let bishop_offset = find_magic::<{ PieceType::Bishop }>(square, &mut BISHOP_MAGICS[square as usize], &mut BISHOP_TABLE[running_bishop_offset..]);
+            let rook_result= find_magic::<{ PieceType::Rook }>(square, &mut ROOK_MAGICS[square as usize], &mut ROOK_TABLE[running_rook_offset..], &mut rng);
+            let bishop_result = find_magic::<{ PieceType::Bishop }>(square, &mut BISHOP_MAGICS[square as usize], &mut BISHOP_TABLE[running_bishop_offset..], &mut rng);
             unsafe { ROOK_MAGICS[square as usize].offset = running_rook_offset as u32 };
             unsafe { BISHOP_MAGICS[square as usize].offset = running_bishop_offset as u32 };
-            running_rook_offset += rook_offset;
-            running_bishop_offset += bishop_offset;
+            running_rook_offset += rook_result.0;
+            running_bishop_offset += bishop_result.0;
+            running_total += rook_result.1 + bishop_result.1;
         }
     }
+    println!("Magics generated in {} passes", running_total);
 }
 
-fn find_magic<const P: PieceType>(square: Square, magic: &mut Magic, magic_table: &mut [Bitboard]) -> usize {
+fn find_magic<const P: PieceType>(square: Square, magic: &mut Magic, magic_table: &mut [Bitboard], rng: &mut Xoroshiro128PlusPlus) -> (usize, u64) {
     match P {
-        PieceType::King | PieceType::Queen | PieceType::Pawn | PieceType::Knight => return 0,
+        PieceType::King | PieceType::Queen | PieceType::Pawn | PieceType::Knight => return (0, 0),
         _ => (),
     }
     let index = P.value();
@@ -74,11 +82,12 @@ fn find_magic<const P: PieceType>(square: Square, magic: &mut Magic, magic_table
     magic.index = bitcount as u8;
     let mut test_table: Vec<u64> = vec![0; table_size];
     let mut found_table = vec![false; table_size];
-    let mut rng = rng();
+    let mut total_passes = 0u64;
     for (i, blockers) in SubsetIterator::new(mask).enumerate() {
         test_table[i] = find_blocked_sliding_attacks::<P>(square, blockers).unwrap();
     }
     loop {
+        total_passes += 1;
         let test_magic = rng.next_u64() & rng.next_u64() & rng.next_u64();
         magic.magic_number = test_magic;
         found_table.fill(false);
@@ -98,7 +107,7 @@ fn find_magic<const P: PieceType>(square: Square, magic: &mut Magic, magic_table
             break;
         }
     }
-    table_size
+    (table_size, total_passes)
 } 
 
 pub fn find_blocked_sliding_attacks<const P: PieceType>(square: Square, mut blockers: Bitboard) -> Option<Bitboard> {
@@ -122,4 +131,38 @@ pub fn get_magic_index(magic: &Magic, occupied: Bitboard) -> usize {
     let blockers = occupied & magic.mask;
     let hash = blockers.wrapping_mul(magic.magic_number);
     ((hash >> (64 - magic.index)) + (magic.offset as u64)) as usize
+}
+
+pub fn find_magic_seed() -> ([u8; 16], u64) {
+    const N: usize = 1000;
+    let mut best = [0u8; 16];
+    let mut best_count = u64::MAX;
+    let mut meta_rng = rand::rng();
+    for _ in 0..N {
+        let contending = &mut [0u8; 16];
+        meta_rng.fill_bytes(contending);
+        println!("{:?}", contending);
+        let mut rng = Xoroshiro128PlusPlus::from_seed(*contending);
+        let mut running_rook_offset: usize = 0;
+        let mut running_bishop_offset: usize = 0;
+        let mut running_total = 0u64;
+        for square in 0..64 {
+            unsafe {
+                unsafe { ROOK_MAGICS[square as usize].offset = 0 };
+                unsafe { BISHOP_MAGICS[square as usize].offset = 0 };
+                let rook_result= find_magic::<{ PieceType::Rook }>(square, &mut ROOK_MAGICS[square as usize], &mut ROOK_TABLE[running_rook_offset..], &mut rng);
+                let bishop_result = find_magic::<{ PieceType::Bishop }>(square, &mut BISHOP_MAGICS[square as usize], &mut BISHOP_TABLE[running_bishop_offset..], &mut rng);
+                unsafe { ROOK_MAGICS[square as usize].offset = running_rook_offset as u32 };
+                unsafe { BISHOP_MAGICS[square as usize].offset = running_bishop_offset as u32 };
+                running_rook_offset += rook_result.0;
+                running_bishop_offset += bishop_result.0;
+                running_total += rook_result.1 + bishop_result.1;
+            }
+        }
+        if running_total < best_count {
+            best_count = running_total;
+            best = *contending;
+        }
+    }
+    (best, best_count)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 #![allow(dead_code)]
 #![allow(unused)]
 
-use crate::{bitboard::{pretty_string_bitboard, pretty_string_square, square_from_string, Bitboard, Square, UNIVERSAL_BITBOARD}, magic::{find_blocked_sliding_attacks, SubsetIterator}, r#move::{build_move, simple_move_from_string}, piece_info::{move_bitboard, movement_info_init, PieceType}};
+use crate::{bitboard::{pretty_string_bitboard, pretty_string_square, square_from_string, Bitboard, Square, UNIVERSAL_BITBOARD}, magic::{find_blocked_sliding_attacks, find_magic_seed, SubsetIterator}, r#move::{build_move, simple_move_from_string}, piece_info::{move_bitboard, movement_info_init, PieceType}};
 
 mod bitboard;
 mod histories;
@@ -14,12 +14,6 @@ mod r#move;
 
 fn main() {
     movement_info_init();
-    let square: Square = 36;
-    let bitboard: Bitboard = 65280;
-    println!("{}", pretty_string_bitboard(find_blocked_sliding_attacks::< { PieceType::Rook }>(square, bitboard).unwrap()));
-    println!("{}", pretty_string_square(square));
-    println!("{}", pretty_string_bitboard(bitboard));
-    println!("{}", pretty_string_bitboard(move_bitboard::< { PieceType::Rook }>(square, bitboard)))
 }
 
 fn bools_to_u64(bits: [bool; 64]) -> Bitboard {
